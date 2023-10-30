cc63287804468cff690a73169b28857048f33a48
Oğuzcan YAVUZ <oguzcanyavuz321@gmail.com>
2023-10-30 21:17:10 +0300

    feat: add line prefabs

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -1,26 +1,23 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Game : MonoBehaviour
 {
 	private Level level;
+	public GameObject linePrefab;
 
-	// Start is called before the first frame update
+
 	void Start()
 	{
 		this.level = new Level();
-	}
+		List<LineSegment> lineSegments = level.shape.lineSegments;
 
-	// Update is called once per frame
-	void Update()
-	{
-		LineRenderer lineRenderer = GetComponent<LineRenderer>();
-		lineRenderer.positionCount = level.shape.points.Count;
-		lineRenderer.widthMultiplier = 0.2f;
-
-		for (int i = 0; i < level.shape.points.Count; i++)
+		for (var i = 0; i < lineSegments.Count; i++)
 		{
-			var point = level.shape.points[i];
-			lineRenderer.SetPosition(i, new Vector3(point.x, point.y, 0));
+			GameObject line = Instantiate(linePrefab, new Vector3(0, 0, 0), Quaternion.identity);
+			LineRenderer lineRenderer = line.GetComponent<LineRenderer>();
+			lineRenderer.SetPosition(0, new Vector3(lineSegments[i].start.x, lineSegments[i].start.y, 0));
+			lineRenderer.SetPosition(1, new Vector3(lineSegments[i].end.x, lineSegments[i].end.y, 0));
 		}
 	}
 }
